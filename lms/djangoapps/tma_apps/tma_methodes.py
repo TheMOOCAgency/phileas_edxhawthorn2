f44120470554b5bfb5511972cc41421b9ed71d9e
root <luciecamilleory@gmail.com>
2019-01-29 12:55:17 +0100

    template certificat charte amundi

--- lms/djangoapps/tma_apps/tma_methodes.py
+++ lms/djangoapps/tma_apps/tma_methodes.py
@@ -7,7 +7,3 @@ def is_tma_staff(user):
     if user.is_active and user.email.find('@themoocagency.com')>-1 and user.is_staff:
         is_tma_staff = True
     return is_tma_staff
-
-
-def gather_course_info(course_id):
-    
