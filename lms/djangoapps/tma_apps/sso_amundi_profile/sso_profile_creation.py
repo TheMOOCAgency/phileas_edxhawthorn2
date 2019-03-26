3cd33cc1a8e709a4c9c2859ce893a9b49505edf5
root <luciecamilleory@gmail.com>
2019-03-26 15:20:15 +0100

    corrections

--- lms/djangoapps/tma_apps/sso_amundi_profile/sso_profile_creation.py
+++ lms/djangoapps/tma_apps/sso_amundi_profile/sso_profile_creation.py
@@ -4,7 +4,7 @@ SOCIAL_AUTH_PIPELINE Additional Step for Amundi
 """
 
 def create_tma_user_profile(backend, user, response, *args, **kwargs):
-    if backend.name ==='amundi':
+    if backend.name =='amundi':
         profile=UserProfile(user=user)
         is_manager=response.get('is_manager')
         if is_manager=="true":
