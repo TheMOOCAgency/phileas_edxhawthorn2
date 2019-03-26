2e14200e38da7c6d0b7dc5e3bf48185ba23966ea
root <luciecamilleory@gmail.com>
2019-03-26 16:04:47 +0100

    correction userprofile

--- lms/djangoapps/tma_apps/sso_amundi_profile/sso_profile_creation.py
+++ lms/djangoapps/tma_apps/sso_amundi_profile/sso_profile_creation.py
@@ -1,17 +1,28 @@
 from student.models import UserProfile
+import json
 """
 SOCIAL_AUTH_PIPELINE Additional Step for Amundi
 """
 
 def create_tma_user_profile(backend, user, response, *args, **kwargs):
     if backend.name =='amundi':
-        profile=UserProfile(user=user)
         is_manager=response.get('is_manager')
+
+        if UserProfile.objects.filter(user=user).exists():
+          profile=UserProfile.objects.get(user=user)
+        else :
+          profile=UserProfile(user=user)
+
+        try :
+            custom_field = json.loads(profile.custom_field)
+        except:
+            custom_field={}    
+
         if is_manager=="true":
             profile.is_manager = True
         else :
             profile.is_manager = False
-        custom_field = profile.get('custom_field',{})
+
         custom_field.update(response)
-        profile.custom_field=custom_field
+        profile.custom_field=json.dumps(custom_field)
         profile.save()
