048695f21d0faedc490582205038821fdff16132
Agathe Raymond-Carlo <agathe.raymond-carlo@themoocagencyc.com>
2019-04-15 09:26:25 +0000

    search - onboarding tag combined with current language

---
+++ lms/djangoapps/tma_apps/legal/views.py
@@ -0,0 +1,18 @@
+from edxmako.shortcuts import render_to_response
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+
+import logging
+
+log = logging.getLogger()
+
+def legal_notice_page(request):
+    context = {}
+    try:
+        if request.LANGUAGE_CODE == 'fr':
+            legal_content = configuration_helpers.get_value('legal_content_fr')
+        else:
+            legal_content = configuration_helpers.get_value('legal_content_en')
+        context.update(legal_content)
+    except:
+        pass
+    return render_to_response('tma_apps/legal.html', context)
