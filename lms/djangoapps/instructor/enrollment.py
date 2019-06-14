e734c5e0bca1a1aafc64da9e15289cedb001e793
Agathe RC <agathe.raymondcarlo@gmail.com>
2019-06-14 12:35:11 +0000

    email template - fix translation

--- lms/djangoapps/instructor/enrollment.py
+++ lms/djangoapps/instructor/enrollment.py
@@ -422,7 +422,7 @@ def get_email_params(course, auto_enroll, secure=True, course_key=None, display_
         "name_text": _("You are invited to follow the training {display_name}").format(display_name=display_name),
         "effort_text": _("Estimated time to complete this training is {effort} minutes.").format(effort=CourseOverview.objects.get(id=course.id).effort),
         "link_text": _("You can access training course by clicking on the following link : {course_url}").format(course_url=course_url),
-        "last_text": _("We wish you a nice training time.)
+        "last_text": _("We wish you a nice training time.")
     }
 
     if TmaCourseOverview.objects.get(course_overview_edx__id=course.id).is_mandatory:
