589a60c91623ac97c7c686cb88623da385c50558
Agathe RC <agathe.raymondcarlo@gmail.com>
2019-07-11 12:07:15 +0000

    invited learners xls - new filters

--- lms/djangoapps/tma_apps/tma_reports/views.py
+++ lms/djangoapps/tma_apps/tma_reports/views.py
@@ -24,80 +24,129 @@ def download_invited_report(request, course_id):
     wb = xlwt.Workbook(encoding='utf-8')
     ws = wb.add_sheet('Users')
 
-    # Sheet header, first row
+    # First row style
     font_style = xlwt.XFStyle()
     font_style.font.bold = True
 
     # Headers 
-    headers_allowed_user = OrderedDict([('enrolled_email', 'Email'), ('time_stamp', 'Email sent')])
+    header_email = OrderedDict([('email', 'Email')])
     headers_user = OrderedDict([('first_name', 'First Name'), ('last_name', 'Last Name')])
     headers_profile = OrderedDict([('rpid', 'RPID'), ('iug', 'IUG')])
     header_customfield = OrderedDict([('zoneinfo', 'Zone Info'), ('societe.id', 'ID Society'), ('societe.name', 'Society Name'), ('societe.description', 'Society Description'), ('service', 'Service')])
-    headers = list(headers_allowed_user.values()) + list(headers_user.values()) + list(headers_profile.values()) + list(header_customfield.values()) + ['Invited/Started']
+    headers = list(header_email.values()) + list(headers_user.values()) + list(headers_profile.values()) + list(header_customfield.values()) + ['Invited']
     # Write headers in row[0]
     for i, header in enumerate(headers):
         ws.write(0, i, header, font_style)
 
-    # Get list of invited users
-    state_transitions = ['from unenrolled to allowed to enroll', 'from unenrolled to enrolled']
-    invited_users = ManualEnrollmentAudit.objects.filter(state_transition__in=state_transitions)
+
+    transition_states = ['from enrolled to enrolled', 'from unenrolled to enrolled']
+
+    # Emails list of people invited by email but not yet registered on the platform when the email was sent
+    invited_users_emails = CourseEnrollmentAllowed.objects.filter(course_id=course_key).values_list('email', flat=True)
+
+    # Emails list of all people enrolled to course, except those we already know that they were invited by email
+    enrolled_users = list(CourseEnrollment.objects.filter(course_id=course_key).exclude(user__email__in=invited_users_emails).values_list('user__email', flat=True))
+
+    # Emails lists of people registered and not enrolled yet
+    enrolled_to_enrolled = []
+    # Emails lists of people registered and already enrolled
+    unenrolled_to_enrolled = []
+
+    for email in enrolled_users:
+        manual_enrollments = ManualEnrollmentAudit.objects.filter(enrollment__course_id=course_key, enrolled_email=email, state_transition__in=transition_states)
+        for me in manual_enrollments:
+            # Those who were already enrolled when the invitation was sent
+            if str(me.enrolled_email) not in enrolled_to_enrolled and me.state_transition == 'from enrolled to enrolled':
+                enrolled_to_enrolled.append(str(me.enrolled_email))
+
+        for me in manual_enrollments:
+            # Those who were not already enrolled when the invitation was sent
+            # It is possible that a n-th invitation was sent to a user who received a n-x invitation. If so, this user is already taken into account in the previous loop (user originally not enrolled then enrolled thanks to an invitation)
+            if str(me.enrolled_email) not in unenrolled_to_enrolled and me.state_transition == 'from unenrolled to enrolled' and str(me.enrolled_email) not in enrolled_to_enrolled:
+                unenrolled_to_enrolled.append(str(me.enrolled_email))
+
     j=0
-    # Generate rows
-    for invited_user in invited_users:
+    # Generate rows for non-existent users
+    for invited_user in invited_users_emails:
+        j+=1
+        # Invited email
+        student_email = str(invited_user)
+        # N/A for all fields and "invited" flag
+        student_user_fields = ["n/a" for field_name in headers_user] + ["n/a" for field_name in headers_profile] + ["n/a" for field_name in header_customfield]
+        invited_or_enrolled = 'Invited'
 
-        # Check only users related to this course_id, whether they exists or not:
-        if CourseEnrollmentAllowed.objects.filter(email=invited_user.enrolled_email, course_id=course_key).exists() or CourseEnrollment.objects.filter(user__email=invited_user.enrolled_email, course_id=course_key).exists() and (invited_user.state_transition in state_transitions):
+        # Combine fields in one list
+        fields = [student_email] + student_user_fields + [invited_or_enrolled]
+    
+        # Write fields in sheet
+        for i, field in enumerate(fields):
+            if isinstance(field, datetime.date):
+                ws.write(j, i, field.strftime("%d-%m-%Y"))
+            else:
+                ws.write(j, i, field)
 
-            log.info(invited_user.enrolled_email)
+    # Generate rows for users already enrolled when the invitation was sent
+    for enrolled_user in enrolled_to_enrolled:
+        j+=1
 
-            j+=1
-            student_allowed_fields = []
-            student_user_fields = []
-            student_customfields = []
-            invited_or_enrolled = ''
-            fields = []
+        student_email = str(enrolled_user)
+        student_user_fields = []
 
-            # If user is not registered yet and was invited to this course
-            if invited_user.state_transition == 'from unenrolled to allowed to enroll':
-                # Get email
-                student_allowed_fields = [getattr(invited_user, field_name) if getattr(invited_user, field_name) else "n/a" for field_name in headers_allowed_user]
+        for user in User.objects.filter(email=enrolled_user):
+            student_user_fields = [getattr(user, field_name) if getattr(user, field_name) else "n/a" for field_name in headers_user]
 
-                # N/A for all fields and "invited" flag
-                student_user_fields = ["n/a" for field_name in headers_user] + ["n/a" for field_name in headers_profile] + ["n/a" for field_name in header_customfield]
-                invited_or_enrolled = 'invited'
+        # Get profile info
+        for profile in UserProfile.objects.filter(user__email=enrolled_user):
+            student_user_fields = student_user_fields + [getattr(profile, field_name) if getattr(profile, field_name) else "n/a" for field_name in headers_profile]
 
-            # If user registered
-            if invited_user.state_transition == 'from allowed to enroll to enrolled':
-                # Get email
-                student_allowed_fields = [getattr(invited_user, field_name) if getattr(invited_user, field_name) else "n/a" for field_name in headers_allowed_user]
+            # Get custom field info 
+            student_json_customfields = {} 
+            try:      
+                student_json_customfields = json.loads(profile.custom_field)
+            except:
+                pass
+            student_user_fields = student_user_fields + [student_json_customfields[field_name] if field_name in student_json_customfields.keys() else "n/a" for field_name in header_customfield]
 
-                # If user already exists and enrolled : get user info
-                for user in User.objects.filter(email=invited_user.enrolled_email):
-                    student_user_fields = [getattr(user, field_name) if getattr(user, field_name) else "n/a" for field_name in headers_user]
+        invited_or_enrolled = 'Invited'
 
-                # Get profile info
-                for profile in UserProfile.objects.filter(user__email=invited_user.enrolled_email):
-                    student_user_fields = student_user_fields + [getattr(profile, field_name) if getattr(profile, field_name) else "n/a" for field_name in headers_profile]
+        fields = [student_email] + student_user_fields + [invited_or_enrolled]
+    
+        for i, field in enumerate(fields):
+            if isinstance(field, datetime.date):
+                ws.write(j, i, field.strftime("%d-%m-%Y"))
+            else:
+                ws.write(j, i, field)
 
-                    # Get custom field info 
-                    student_json_customfields = {} 
-                    try:      
-                        student_json_customfields = json.loads(profile.custom_field)
-                    except:
-                        pass
-                    student_customfields = [student_json_customfields[field_name] if field_name in student_json_customfields.keys() else "n/a" for field_name in header_customfield]
 
-                invited_or_enrolled = 'started'
+    # Generate rows for users not already enrolled when the invitation was sent
+    for enrolled_user in unenrolled_to_enrolled:
+        j+=1
 
-            # Combine fields in one list
-            fields = student_allowed_fields + student_user_fields + student_customfields + [invited_or_enrolled]
-        
-            # Write fields in sheet
-            for i, field in enumerate(fields):
-                if isinstance(field, datetime.date):
-                    ws.write(j, i, field.strftime("%d-%m-%Y"))
-                else:
-                    ws.write(j, i, field)
+        student_email = str(enrolled_user)
+        student_user_fields = []
+
+        for user in User.objects.filter(email=enrolled_user):
+            student_user_fields = [getattr(user, field_name) if getattr(user, field_name) else "n/a" for field_name in headers_user]
+
+        for profile in UserProfile.objects.filter(user__email=enrolled_user):
+            student_user_fields = student_user_fields + [getattr(profile, field_name) if getattr(profile, field_name) else "n/a" for field_name in headers_profile]
+
+            student_json_customfields = {} 
+            try:      
+                student_json_customfields = json.loads(profile.custom_field)
+            except:
+                pass
+            student_user_fields = student_user_fields + [student_json_customfields[field_name] if field_name in student_json_customfields.keys() else "n/a" for field_name in header_customfield]
+
+        invited_or_enrolled = 'Invited'
+
+        fields = [student_email] + student_user_fields + [invited_or_enrolled]
+    
+        for i, field in enumerate(fields):
+            if isinstance(field, datetime.date):
+                ws.write(j, i, field.strftime("%d-%m-%Y"))
+            else:
+                ws.write(j, i, field)
 
     wb.save(response)
     return response
