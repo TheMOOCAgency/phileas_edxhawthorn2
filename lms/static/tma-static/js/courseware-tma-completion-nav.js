947a6819c7eeb65b3051f674480c4fb3d70c8807
agathe-rc <agathe.raymondcarlo@gmail.com>
2019-08-27 12:47:55 +0000

    course completion - disable next button if unit not completed

--- lms/static/tma-static/js/courseware-tma-completion-nav.js
+++ lms/static/tma-static/js/courseware-tma-completion-nav.js
@@ -116,27 +116,25 @@ function highlight_current_unit(){
 // Conditional access to next section
 function isUnitAvailable() {
   currentUnit = $('.xblock.xblock-student_view.xblock-student_view-vertical.xblock-initialized').data('usage-id');
+
   $('ol.outline-item.accordion-panel li a.outline-item.focusable').each(function(i, element){
     isUnitSeen = ($(this).children().children().hasClass('tma_completed') || $(this).children().children().hasClass('tma_started'))
     isPreviousSeen = ($('ol.outline-item.accordion-panel li a.outline-item.focusable').eq(i - 1).children().children().hasClass('tma_completed') || $('ol.outline-item.accordion-panel li a.outline-item.focusable').eq(i - 1).children().children().hasClass('tma_started'))
+
     // If current unit and previous unit have not been started nor completed, link is disabled
     if (!isUnitSeen && !isPreviousSeen) {
       $(this).addClass('disabled-link');
     }
-
-    if ($(this).hasClass('tma-current-unit') && !$(this).children().children().hasClass('tma_completed')) {
-      $('.sequence-nav-button.button-next').prop('disabled',)
-    }
-
-    if ($('ol.outline-item.accordion-panel li a.outline-item.focusable.tma-current-unit').children().children().attr('id') == currentUnit) {
-      $('.sequence-bottom .sequence-nav-button.button-next').prop('disabled') 
-    }
   });
 
-  
-  
+  // If current unit is not completed yet, disable button next + add tooltip
+  if (!$('ol.outline-item.accordion-panel li a.outline-item.focusable.tma-current-unit').children().children().hasClass('tma_completed')) {
+    $('.sequence-bottom .sequence-nav-button.button-next').prop('disabled', true);
+    $('.sequence-bottom .sequence-nav-button.button-next').attr('data-toggle', 'tooltip');
+    $('.sequence-bottom .sequence-nav-button.button-next').attr('title', 'You cannot access the next unit because the current unit is not completed. Please ensure that you viewed all contents and answered all questions.');
+  }
 
-  console.log(currentUnit)
-  console.log(decodeURIComponent(window.location.search))
+  //console.log(currentUnit)
+  //console.log(decodeURIComponent(window.location.search))
   //$('.sequence-nav-button.button-next').prop('disabled')
 }
