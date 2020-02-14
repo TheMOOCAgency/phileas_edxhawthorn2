e854ffbc75cf8d0b79f6e9c4b673b70b241f5848
jcbouchaud <jean-charles.bouchaud@themoocagency.com>
2020-02-14 15:30:07 +0100

    courses/programs urls views creation

--- lms/static/tma-static/js/popup-bravo.js
+++ lms/static/tma-static/js/popup-bravo.js
@@ -1,91 +1,96 @@
 $(document).ajaxSuccess(function(e, xhr, settings) {
   // Course with quiz or SCORM
-  if (settings.url.indexOf('get_user_grade') >- 1) {
+  if (settings.url.indexOf("get_user_grade") > -1) {
     response = JSON.parse(xhr.responseText);
-    console.log(response)
+    console.log(response);
     // If course is completed
-    if (response['has_completed'] && !response['has_displayed_message']) {
+    if (response["has_completed"] && !response["has_displayed_message"]) {
       // If course is graded & success
-      if (response['graded_success']) {
-        console.log('cours noté - bravo')
-        certificate_url = '/tma_apps/'+global_courseid+"/certificate/render"
-        $('.modal-graded-success a').attr("href",certificate_url)
-        $('.modal-graded-success .score').html(response['user_grade']*100)
-        $('.modal-graded-success').show();
-        $('#tma_course_end_popup').modal('show')
-        mark_popup_as_displayed('True');
+      if (response["graded_success"]) {
+        console.log("cours noté - bravo");
+        certificate_url =
+          "/tma_apps/" + global_courseid + "/certificate/render";
+        $(".modal-graded-success a").attr("href", certificate_url);
+        $(".modal-graded-success .score").html(response["user_grade"] * 100);
+        $(".modal-graded-success").show();
+        $("#tma_course_end_popup").modal("show");
+        mark_popup_as_displayed("True");
       } else {
         // If failure
-        console.log('cours noté - sorry')
-        reset_url = ""
-        $('.modal-graded-fail a').attr("href",reset_url)
-        $('.modal-graded-fail .score').html(response['user_grade']*100)
+        console.log("cours noté - sorry");
+        reset_url = "";
+        $(".modal-graded-fail a").attr("href", reset_url);
+        $(".modal-graded-fail .score").html(response["user_grade"] * 100);
         // If quiz has limited attempts : no retry possible
         if (!canRetry) {
-          $('#try_again').hide();
+          $("#try_again").hide();
         }
-        $('.modal-graded-fail').show();
-        $('#tma_course_end_popup').modal('show')
-        mark_popup_as_displayed('True');
+        $(".modal-graded-fail").show();
+        $("#tma_course_end_popup").modal("show");
+        mark_popup_as_displayed("True");
       }
     } else {
       // If course is not graded && completed
-      if (response['not_graded_success'] && !response['has_displayed_message']) {
-        console.log('cours non noté - bravo');
-        $('.modal-not-graded-success').show();
-        $('#tma_course_end_popup').modal('show');
-        mark_popup_as_displayed('True');
+      if (
+        response["not_graded_success"] &&
+        !response["has_displayed_message"]
+      ) {
+        console.log("cours non noté - bravo");
+        $(".modal-not-graded-success").show();
+        $("#tma_course_end_popup").modal("show");
+        mark_popup_as_displayed("True");
       }
     }
   }
 });
 
-
-$('button.mark-as-done').on('click', function(e){
+$("button.mark-as-done").on("click", function(e) {
   e.preventDefault();
   mark_as_done();
-  $('#tma_course_end_popup').modal('hide');
+  $("#tma_course_end_popup").modal("hide");
 });
 
 // If student has failed clicks "Start over" button
-$('#try_again').on('click', function(e){
+$("#try_again").on("click", function(e) {
   e.preventDefault();
   $.ajax({
-    type:'POST',
-    url: '/tma_apps/'+global_courseid+'/grade_tracking/try_again',
+    type: "POST",
+    url: "/tma_apps/" + global_courseid + "/grade_tracking/try_again",
     data: {
-      'message_displayed_status': 'False'
+      message_displayed_status: "False"
     },
-    success : function(response) {
+    success: function(response) {
       console.log(response);
-      $('#tma_course_end_popup').modal('hide');
-      mark_popup_as_displayed('False');
+      $("#tma_course_end_popup").modal("hide");
+      mark_popup_as_displayed("False");
       location.reload();
     }
-  })
+  });
 });
 
-function mark_popup_as_displayed(status){
-  url ='/tma_apps/'+global_courseid+'/grade_tracking/message_displayed'
+function mark_popup_as_displayed(status) {
+  url = "/tma_apps/" + global_courseid + "/grade_tracking/message_displayed";
   $.ajax({
-    type:'post',
-    url:url,
-    data:{
-      'message_displayed_status': status
+    type: "post",
+    url: url,
+    data: {
+      message_displayed_status: status
     }
   });
 }
 
 function mark_as_done() {
-  url ='/tma_apps/'+global_courseid+'/grade_tracking/mark_as_done'
+  url = "/tma_apps/" + global_courseid + "/grade_tracking/mark_as_done";
   $.ajax({
-    type: 'POST',
+    type: "POST",
     url: url,
     data: {
-      'marked_as_done':'True'
+      marked_as_done: "True"
     },
-    success: function(){
-      window.location.assign("/tma_apps/dashboard/home/#finished-section");
+    success: function() {
+      window.location.assign(
+        "/tma_apps/dashboard/home/courses/#finished-section"
+      );
     }
   });
 }
