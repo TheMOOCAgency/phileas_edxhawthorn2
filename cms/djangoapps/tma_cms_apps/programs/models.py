7a7f62d3dc41bc562f5a571be6fbb2240014bf2f
jcbouchaud <jean-charles.bouchaud@themoocagency.com>
2020-07-06 14:28:20 +0200

    moved program app from lms to cms and fixed logs issues

---
+++ cms/djangoapps/tma_cms_apps/programs/models.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from student.models import CourseEnrollment
+from django.contrib.auth.models import User
+from datetime import datetime
+import logging
+
+log = logging.getLogger()
+
+class TmaProgramEnrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField(default=datetime.now())
+    has_started_program = models.BooleanField(default=False)
+    has_validated_program = models.BooleanField(default=False)
+    program_completion_rate = models.FloatField(default=0)
+
+class TmaProgramOverview(models.Model):
+    is_manager_only = models.BooleanField(default=False)
+    is_mandatory = models.BooleanField(default=False)
+    is_linear = models.BooleanField(default=False)
+    invitation_only = models.BooleanField(default=False)
+    is_new = models.BooleanField(default=False)
+    program_start_date = models.DateTimeField(null=True)
+    program_due_date = models.DateTimeField(null=True)
+
+class TmaProgramCourse(models.Model):
+    program = models.ForeignKey(TmaProgramEnrollment, on_delete=models.CASCADE, unique=True)
+    course = models.ForeignKey(CourseEnrollment, on_delete=models.CASCADE, unique=True)
+    order = models.IntegerField(default=0)
---
+++ cms/djangoapps/tma_cms_apps/programs/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from .helpers import TmaProgramManager
+
+
+
+@login_required
+@require_http_methods(["POST"])
+def create_program(request):
+    program_data = request.POST.copy()
+    tma_program_creator = TmaProgramManager(request, program_data)
+    new_program = tma_program_creator.create_new_program()
+
+    status = 400 if new_program['status'] == 'error' else 200
+
+    return JsonResponse(new_program, status = status)
